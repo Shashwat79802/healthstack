8c0876d4760cd6fbeee32fbd7c585ebf85e6b7c5
poorvadiwan <diwanpoorva07@gmail.com>
2024-04-19 23:49:50 +0530

    message

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,82 +1,58 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     // Fetch data from the JSON file
-//     fetch('data/hospital-reports.json')
-//         .then(response => response.json())
-//         .then(data => {
-//             // Extract data from the JSON
-//             const dates = Object.values(data).map(item => item.Date);
-//             const bloodGlucoseFasting = Object.values(data).map(item => item['Blood Glucose(F)']);
-//             const bloodGlucosePostprandial = Object.values(data).map(item => item['Blood Glucose(PP)']);
+function updateChartWithData(data, analysisType) {
+    if (analysisType === 'blood_count') {
+        updateBloodCountChart(data);
+    } else if (analysisType === 'blood_glucose') {
+        updateBloodGlucoseChart(data);
+    } else {
+        console.error('Invalid analysis type:', analysisType);
+    }
+}
 
-//             // Create the chart
-//             const ctx = document.getElementById('bloodGlucoseChart').getContext('2d');
-//             const bloodGlucoseChart = new Chart(ctx, {
-//                 type: 'line',
-//                 data: {
-//                     labels: dates,
-//                     datasets: [{
-//                         label: 'Blood Glucose (Fasting)',
-//                         data: bloodGlucoseFasting,
-//                         backgroundColor: 'purple',
-//                         borderColor: 'purple',
-//                         fill: false
-//                     }, {
-//                         label: 'Blood Glucose (Postprandial)',
-//                         data: bloodGlucosePostprandial,
-//                         backgroundColor: 'pink',
-//                         borderColor: 'pink',
-//                         fill: false
-//                     }]
-//                 },
-//                 options: {
-//                     scales: {
-//                         yAxes: [{
-//                             scaleLabel: {
-//                                 display: true,
-//                                 labelString: 'Blood Glucose Levels'
-//                             }
-//                         }],
-//                         xAxes: [{
-//                             scaleLabel: {
-//                                 display: true,
-//                                 labelString: 'Date'
-//                             }
-//                         }]
-//                     }
-//                 }
-//             });
-//         });
-// });
+function updateBloodGlucoseChart(data) {
+    const dates = Object.values(data).map(item => item.Date);
+    const bloodGlucoseFasting = Object.values(data).map(item => item['Blood Glucose(F)']);
+    const bloodGlucosePostprandial = Object.values(data).map(item => item['Blood Glucose(PP)']);
 
+            // Create the chart
+    const ctx = document.getElementById('bloodGlucoseChart').getContext('2d');
+        const medicalReportsChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: dates,
+                datasets: [{
+                    label: 'Blood Glucose (Fasting)',
+                    data: bloodGlucoseFasting,
+                    backgroundColor: 'purple',
+                    borderColor: 'purple',
+                    fill: false
+                }, {
+                    label: 'Blood Glucose (Postprandial)',
+                    data: bloodGlucosePostprandial,
+                    backgroundColor: 'pink',
+                    borderColor: 'pink',
+                    fill: false
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Blood Glucose Levels'
+                        }
+                    }],
+                    xAxes: [{
+                        scaleLabel: {
+                            display: true,
+                            labelString: 'Date'
+                        }
+                    }]
+                }
+            }
+        });
+    };
 
-
-function updateChartWithData(data) {
-    // Fetch data from the JSON file
-    // fetch('data/hospital-reports.json')
-    //     .then(response => response.json())
-    //     .then(data => { - the rest of the code goes here
-    //      });
-
-    // const data = {
-    //     "0": {
-    //         "Haemoglobin": 10.1,
-    //         "Platelet Count": 3.0,
-    //         "RBC": 4.5,
-    //         "Date": "08/14/2024"
-    //     },
-    //     "1": {
-    //         "Haemoglobin": 8.0,
-    //         "Platelet Count": 5.0,
-    //         "RBC": 4.5,
-    //         "Date": "08/14/2024"
-    //     },
-    //     "2": {
-    //         "Haemoglobin": 9.4,
-    //         "Platelet Count": 5.0,
-    //         "RBC": 4.2,
-    //         "Date": "08/14/2024"
-    //     }
-    // };
+function updateBloodCountChart(data) {
 
             // Extract data from the JSON
             const dates = Object.values(data).map(item => item.Date);
